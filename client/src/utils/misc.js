a80cee451d4a5427292d4de712763e8c2475bc1c
ScottE <scotteratigan@gmail.com>
2019-02-28 15:47:49 -0800

    Added a stock search component with autocomplete.

--- client/src/utils/misc.js
+++ client/src/utils/misc.js
@@ -1,14 +1,6 @@
-
 function formatCash(pennies) {
   // accepts input in pennies, outputs as formatted string: $ 1,534.99
   return (pennies / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 }
 
 module.exports = { formatCash };
-// formatCash(100);
-// formatCash(123);
-// formatCash(-1234);
-// formatCash(12345);
-// formatCash(123456);
-// formatCash(-1234567);
-// console.log(formatCash(10000000));
