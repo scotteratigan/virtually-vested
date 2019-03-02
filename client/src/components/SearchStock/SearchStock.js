875a2d68f484789c9442477ea8a3055a950ad0cb
Scott Ratigan <scotteratigan@gmail.com>
2019-03-02 14:18:13 -0800

    actually added search bar this time

---
+++ client/src/components/SearchStock/SearchStock.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { ALPHAVANTAGE_API_KEY } from '../../utils/secret.json';
+
+//todo - add company name w/ special delimiter char
+
+class SearchStocks extends Component {
+	state = {
+		searchTerm: '',
+		tickerSearchResults: []
+	}
+
+	handleAddStockBtn = event => {
+		event.preventDefault();
+		alert('You selected: ' + this.state.searchTerm);
+	}
+
+	handleKeyInput = async (event) => {
+		console.log(event.nativeEvent);
+		// when you type, nativeEvent is an InputEvent and has a data attribute
+		// when you click, nativeEvent is an Event and has type: input
+		await this.setState({ searchTerm: event.target.value });
+		const searchURL = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${this.state.searchTerm}&apikey=${ALPHAVANTAGE_API_KEY}`;
+		axios.get(searchURL).then(res => {
+			// todo: don't update list user clicks on suggestion
+			this.setState({ tickerSearchResults: res.data.bestMatches });
+		});
+	}
+
+	render() {
+		console.log();
+		return (
+			<form className="search">
+				<div className="form-group">
+					<label htmlFor="company">company Name:</label>
+					<input
+						value={this.state.searchTerm}
+						onChange={this.handleKeyInput}
+						name="company"
+						list="companies"
+						type="text"
+						className="form-control"
+						placeholder="Type in company name or stock ticker to begin..."
+						id="company"
+						autoComplete="off"
+					/>
+					<datalist id="companies">
+						{this.state.tickerSearchResults.length > 1 ? this.state.tickerSearchResults.map(company => (
+							<option value={company['1. symbol']} key={company['1. symbol']} onClick={() => alert('hell yeah')} />
+						)) : ''}
+					</datalist>
+					<button onClick={this.handleAddStockBtn} className="btn btn-success">Add Stock</button>
+				</div>
+			</form>
+		);
+	}
+}
+
+export default SearchStocks;
