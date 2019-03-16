139d2a8db2db4a5ace56158c0eae4a5d94edc8c0
Pat Rice <patrickrice@Patricks-MBP.hsd1.ca.comcast.net>
2019-03-16 04:39:04 -0700

    links added to landing page

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -7,7 +7,11 @@ import Manage from '../images/img5.jpg';
 import Research from '../images/img6.jpg';
 import Logo from '../images/logo.png';
 import { Col, Row, Container } from '../components/Grid';
+import { Link } from 'react-router-dom';
+import LogIn from '../components/LogIn/LogIn';
+import LogOut from '../components/LogOut/LogOut';
 
+const anchorRef = React.createRef(LogIn);
 
 class Index extends Component {
   render() {
@@ -24,16 +28,20 @@ class Index extends Component {
               <h1 style={{ color: 'white', textShadow: '0 0 5px green', textDecoration: 'underline' }}>The Real-time Virtual Stock Trading App</h1>
               <br /><br />
               <span style={{ float: 'none', padding: '30px' }}>
-                <h3 style={{ color: 'white', textShadow: '0 0 3px green', textDecoration: 'underline', padding: '10px' }}>Begin New Simulation</h3>
-                <img src={Start} style={{ backgroundColor: 'black', padding: '20px' }} alt='Start' /><br />
+                <Link to='/portfolio'>
+                  <h3 style={{ color: 'white', textShadow: '0 0 3px green', textDecoration: 'underline', padding: '10px' }}>Begin New Simulation</h3>
+                  <img src={Start} style={{ backgroundColor: 'black', padding: '20px' }} alt='Start' /><br />
+                </Link>
               </span>
               <span style={{ display: 'block', float: 'left', padding: '40px' }}>
                 <h3 style={{ textShadow: '0 0 3px green', textDecoration: 'underline' }}>Manage my Portfolio</h3>
                 <img src={Manage} style={{ backgroundColor: 'white' }} alt='Manage' /><br />
               </span>
               <span style={{ display: 'inline', float: 'right', padding: '40px' }}>
-                <h3 style={{ textShadow: '0 0 3px green', textDecoration: 'underline' }}>Research</h3>
-                <img src={Research} style={{ backgroundColor: 'white' }} alt='Research' /><br />
+                <Link to='/stockhistory'>
+                  <h3 style={{ textShadow: '0 0 3px green', textDecoration: 'underline' }}>Research</h3>
+                  <img src={Research} style={{ backgroundColor: 'white' }} alt='Research' /><br />
+                </Link>
               </span>
             </div>
           </Col>
